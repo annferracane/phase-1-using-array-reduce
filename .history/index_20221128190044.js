75bcac41a3ec79ca82ec6b107bed3f40db5c3905
Ann Ferracane <ann@patchventures.com>
2022-11-28 19:01:45 -0500

    commit tree 3e4222eeec0f7c8a18aa7d6cec08207943d110cc

---
+++ .history/index_20221128190044.js
@@ -0,0 +1,7 @@
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// Code your solution here
+const totalBatteries = batteryBatches.reduce(fn(accumulator, element){ return  + accumulator}, 0);
+
+const doubledAndSummedFromTen = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 10)
+// => 22
